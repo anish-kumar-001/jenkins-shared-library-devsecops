a0b5d31c6a650fa6ce9886fdc0377c9447b9fd42
anish-kumar-001 <choudharyanish078@gmail.com>
2025-11-16 17:48:58 +0530

    Add shared library with reusable pipeline functions

--- vars/gitleaksScan.groovy
+++ vars/gitleaksScan.groovy
@@ -1,3 +1,3 @@
 def call(){
-  sh "docker run --rm -v $(pwd):/repo zricethezav/gitleaks:latest detect --source=/repo --no-banner --config=/repo/.gitleaks.toml || true"
+  sh 'docker run --rm -v $(pwd):/repo zricethezav/gitleaks:latest detect --source=/repo --no-banner --config=/repo/.gitleaks.toml || true'
 }
--- vars/semgrepScan.groovy
+++ vars/semgrepScan.groovy
@@ -1,3 +1,3 @@
 def call(){
-  sh "docker run --rm -v $(pwd):/src returntocorp/semgrep semgrep --config=auto /src || true"
+  sh 'docker run --rm -v $(pwd):/src returntocorp/semgrep semgrep --config=auto /src || true'
 }
